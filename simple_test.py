41317e02af82e4cb52b7e7702700799f3452f388
KWood <kent.wood@nutonian.com>
2018-10-20 22:26:20 -0400

    update

--- simple_test.py
+++ simple_test.py
@@ -13,5 +13,11 @@ def test_the_search():
     python_org_page.search_for("pycon")
     assert "No results found." not in python_org_page.get_page_source()
 
+def test_the_search_again():
+    assert "Python" in driver.title
+    python_org_page = PythonOrgPage(driver)
+    python_org_page.search_for("pycon")
+    assert "PyCon Argentina" in python_org_page.get_page_source()
+
 def teardown_module(module):
     driver.quit()
