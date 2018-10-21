2ce9bf9f7a76f09dd91a1a5acafe6f512cd067e9
KWood <kent.wood@nutonian.com>
2018-10-21 00:07:24 -0400

    update

--- python_org_page.py
+++ python_org_page.py
@@ -3,9 +3,6 @@ from selenium.webdriver.common.by import By
 
 class PythonOrgPage(BasePage):
 
-    def __init__(self, driver):
-        self.driver = driver
-
     search_input = (By.NAME, "q")
     submit_button = (By.CSS_SELECTOR, "button#submit")
 
